b0dd31947c2c8d1589bff8635512d41869dc3242
leellfly <20855698+leellfly@users.noreply.github.com>
2023-11-27 15:33:58 +0800

    add solution to problem 5

---
+++ problems/5.ts
@@ -0,0 +1,33 @@
+/*
+    Smallest Multiple
+
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any
+    remainder.
+
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+function solve(maxNum: number): number {
+    const gcd = (a: number, b: number): number => b === 0 ? a : gcd(b, a % b)
+
+    const lcm = (a: number, b: number): number => (a * b) / gcd(a, b)
+
+    const arr = Array.from({ length: maxNum }, (_, index) => index + 1)
+
+    const smallestMultiple = arr.reduce((lcmValue, currentValue) => {
+        return lcm(lcmValue, currentValue)
+    }, 1)
+
+    return smallestMultiple
+}
+
+console.log(solve(20));//232792560
+
+
+export { }
+/**
+ * Note:
+ *  「GCD」:greatest common divisor
+ *  「LCM」：least common multiple
+ *   GCD*LCM = a*b => LCM = a*b / GCD    
+ */
