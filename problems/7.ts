f20443273f1690ccd9387876063de6df0b4d45d5
leellfly <20855698+leellfly@users.noreply.github.com>
2023-11-29 11:03:23 +0800

    add solution of problem 7

---
+++ problems/7.ts
@@ -0,0 +1,50 @@
+/*
+    10001st Prime
+
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+    What is the 10001st prime number?
+*/
+
+function solve(targetPosition: number): number {
+    if (targetPosition < 1) {
+        throw new Error("Target position should be a positive integer");
+    }
+
+    const isPrime = (n: number) => {
+        if (n <= 1) {
+            return false
+        }
+        if (n == 2) {
+            return true
+        }
+
+        const sqrtN = Math.sqrt(n)
+        for (let j = 2; j <= sqrtN; j++) {
+            if (n % j === 0) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    let count: number = 0
+    let i = 1
+
+    while (count < targetPosition) {
+        if (isPrime(i)) {
+            count++
+        }
+        if (count < targetPosition) {
+            i++
+        }
+    }
+
+    return i
+}
+
+console.log(solve(10001))//104743
+
+
+export { }
