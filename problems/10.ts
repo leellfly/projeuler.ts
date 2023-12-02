8824c16059fa7fee27d6077b32d0b858bc857876
leellfly <20855698+leellfly@users.noreply.github.com>
2023-12-02 23:46:58 +0800

    add solution of problem 10

---
+++ problems/10.ts
@@ -0,0 +1,55 @@
+/*
+    Summation of Primes
+
+    The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+    Find the sum of all the primes below two million.
+ */
+
+function solve(maxPrime: number): number {
+    const sieve = Array(maxPrime).fill(true)
+    sieve[0] = false
+    sieve[1] = false
+
+    for (let i = 2; i <= Math.sqrt(maxPrime); i++) {
+        if (sieve[i]) {
+            for (let j = i * i; j < maxPrime; j += i) {
+                sieve[j] = false
+            }
+        }
+    }
+
+    let sum: number = 0
+    for (let i = 2; i < maxPrime; i += (i === 2) ? 1 : 2) {
+        if (sieve[i]) {
+            sum += i
+        }
+    }
+
+    return sum
+}
+
+const maxPrime = 2000000
+console.log(solve(maxPrime))//142913828922
+
+
+export { }
+/*
+
+    Utilize the optimized "Sieve of Eratosthenes" in the improved version of the typical isPrime function.
+
+    The fundamental concept of this algorithm involves systematically eliminating non-prime numbers, ultimately retaining only the prime numbers in the end.
+
+    const isPrime = (i: number): boolean => {
+        const sqrI = Math.sqrt(i)
+
+        for (let j = 2; j <= sqrI; j++) {
+            if (i % j == 0) {
+                return false
+            }
+        }
+
+        return true
+    }
+*/
+
