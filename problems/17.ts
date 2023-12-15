49fb57207e926bea38d10f0a513d2c4a56f10212
leellfly <20855698+leellfly@users.noreply.github.com>
2023-12-15 23:03:00 +0800

    format code

--- problems/17.ts
+++ problems/17.ts
@@ -26,34 +26,34 @@ function solve(): number {
         6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety',
     };
 
-    const hundredsDigit = 'hundred';
-    const thousandDigit = 'thousand';
-    const and = 'and';
+    const hundredsDigit = 'hundred'
+    const thousandDigit = 'thousand'
+    const and = 'and'
 
     const getWordLength = (num: number): number => {
         if (num === 1000) {
-            return unitsDigit[1].length + thousandDigit.length;
+            return unitsDigit[1].length + thousandDigit.length
         } else if (num >= 100) {
-            const hundreds = Math.floor(num / 100);
-            const remainder = num % 100;
-            const andLength = remainder !== 0 ? and.length : 0;
-            return unitsDigit[hundreds].length + hundredsDigit.length + (andLength ? andLength + getWordLength(remainder) : 0);
+            const hundreds = Math.floor(num / 100)
+            const remainder = num % 100
+            const andLength = remainder !== 0 ? and.length : 0
+            return unitsDigit[hundreds].length + hundredsDigit.length + (andLength ? andLength + getWordLength(remainder) : 0)
         } else if (num >= 20) {
-            const tens = Math.floor(num / 10);
-            const units = num % 10;
-            return tensDigit[tens].length + (units !== 0 ? unitsDigit[units].length : 0);
+            const tens = Math.floor(num / 10)
+            const units = num % 10
+            return tensDigit[tens].length + (units !== 0 ? unitsDigit[units].length : 0)
         } else {
-            return unitsDigit[num].length;
+            return unitsDigit[num].length
         }
-    };
-
-    let sum = 0;
-
-    for (let i = 1; i <= 1000; i++) {
-        sum += getWordLength(i);
     }
 
-    return sum;
+    let sum = 0
+
+    for (let i = 1; i <= 1000; i++) {
+        sum += getWordLength(i)
+    }
+
+    return sum
 }
 
 console.log(solve());//21124
