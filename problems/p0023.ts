9e3c7cb7c62450d68574e99548b173e1f7e7e0ff
leellfly <20855698+leellfly@users.noreply.github.com>
2024-02-03 17:39:05 +0800

    add solution of problem 23

---
+++ problems/p0023.ts
@@ -0,0 +1,64 @@
+/*
+  Non-Abundant Sums
+
+  A perfect number is a number for which the sum of its proper divisors is exactly equal to the
+  number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which
+  means that 28 is a perfect number.
+
+  A number n is called deficient if the sum of its proper divisors is less than n and it is called
+  abundant if this sum exceeds n.
+
+  As 12 is the smallest abundant number, 1 + 2 + 3 + 6 = 16, the smallest number that can be writaten
+  as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all
+  integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper
+  limit cannot be reduced any further by analysis even though it is known that the greatest number
+  that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+  Find the sum of all the positive integers which cannot be written as the sum of two abundant
+  numbers.
+*/
+
+import { measureTime } from "../utils"
+
+function solve() {
+
+  const calculateProperDivisorsSum = (n: number): number => {
+    let sum = 1// 1 is a proper divisor for all numbers
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        sum += i
+        if (i !== n / i) sum += n / i
+      }
+    }
+    return sum
+  }
+
+  //check if the sum of its proper divisors is exactly exceeds to the number
+  const isAbundantNumber = (n: number) => {
+    return calculateProperDivisorsSum(n) > n
+  }
+
+  const isSumOfTwoAbundantNumbers = (n: number): boolean => {
+    console.log(n, 'n');
+    for (let i = 1; i <= n / 2; i++) {
+      if (isAbundantNumber(i) && isAbundantNumber(n - i)) {
+        return true
+      }
+    }
+    return false
+  }
+
+  let nonAbundantSums = 0
+
+  for (let i = 1; i <= 28123; i++) {
+    if (!isSumOfTwoAbundantNumbers(i)) {
+      nonAbundantSums += i
+    }
+  }
+
+  return nonAbundantSums
+}
+
+const [result, elapsedTime] = measureTime(() => solve())
+console.log('result', result)//4179871
+console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
