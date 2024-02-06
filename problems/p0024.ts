75e2a4fd27bff7f3062a5d843cb84ae3859caef1
leellfly <20855698+leellfly@users.noreply.github.com>
2024-02-06 22:15:26 +0800

    add desc about problem 24

---
+++ problems/p0024.ts
@@ -0,0 +1,41 @@
+/*
+  Lexicographic Permutations
+
+  A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation
+  of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically,
+  we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+              012   021   102   120   201   210
+
+  What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+*/
+
+import { measureTime } from "../utils"
+
+function solve(): Array<number> {
+  const digits: Array<number> = [0, 1, 2];
+  /**
+   * 012
+   * 021
+   * 102
+   * 120
+   * 201
+   * 210
+   */
+  const res: Array<number> = [];
+
+  for (let i = 0; i < digits.length; i++) {
+    for (let j = 0; j < digits.length; j++) {
+      if (i !== j) {
+
+      }
+    }
+  }
+
+  return res;
+}
+
+
+const [result, elapsedTime] = measureTime(() => solve())
+console.log('result', result)
+console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
