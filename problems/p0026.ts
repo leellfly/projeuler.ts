459e31f6f65f47225acceb6e6e25a17271b62cdb
leellfly <20855698+leellfly@users.noreply.github.com>
2024-02-11 23:21:34 +0800

    add solution for 26

--- problems/p0026.ts
+++ problems/p0026.ts
@@ -23,10 +23,35 @@
 
 import { measureTime } from "../utils"
 
-function solve() {
+function solve(limit:number):number {
+  let maxLength = 0
+  let resultD = 0
 
+  for (let d = 2; d < limit; d++) {
+    let remainder = 1
+    let remainders = new Array(d).fill(0)
+
+    for (let position = 0; position < d; position++) {
+      remainder = (remainder * 10) % d
+
+      if (remainders[remainder] !== 0) {
+        const cycleLength = position - remainders[remainder] + 1
+
+        if (cycleLength > maxLength) {
+          maxLength = cycleLength
+          resultD = d
+        }
+
+        break
+      }
+
+      remainders[remainder] = position + 1
+    }
+  }
+
+  return resultD;
 }
 
-const [result, elapsedTime] = measureTime(() => solve())
-console.log('result', result)
+const [result, elapsedTime] = measureTime(() => solve(1000))
+console.log('result', result)//983
 console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
