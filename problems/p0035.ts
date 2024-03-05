9dbcccaa4822d35ac763ebff6062257b88f762ea
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-05 10:53:12 +0800

    add solution of problem 35

--- problems/p0035.ts
+++ problems/p0035.ts
@@ -13,9 +13,57 @@
 import { measureTime } from "../utils"
 
 function solve() {
+  const isPrime = (n: number) => {
+    if (n <= 1) {
+      return false
+    }
 
+    if (n == 2) {
+      return true
+    }
+
+    const middleN = n / 2
+    for (let j = 2; j < middleN; j++) {
+      if (n % j === 0) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  const generateRotations = (number: number): number[] => {
+    const numberStr = number.toString()
+    const rotations: number[] = []
+
+    for (let i = 0; i < numberStr.length; i++) {
+      const rotatedNumberStr = numberStr.slice(i) + numberStr.slice(0, i)
+      const rotatedNumber = parseInt(rotatedNumberStr, 10)
+      rotations.push(rotatedNumber)
+    }
+
+    return rotations
+  }
+
+  let count = 0
+  const memo: Map<number, boolean> = new Map()
+
+  for (let i = 1; i < 1000000; i++) {
+    const rotations = generateRotations(i)
+    const isSatisfied = rotations.every(item => {
+      if (!memo.has(item)) {
+        memo.set(item, isPrime(item))
+      }
+      return memo.get(item)!
+    })
+    if (isSatisfied) {
+      count++
+    }
+  }
+
+  return count
 }
 
 const [result, elapsedTime] = measureTime(() => solve())
-console.log('result', result)
+console.log('result', result)//56
 console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
