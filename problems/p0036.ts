e0c31d04d041ccaa505537603f0e61ca9ed7789c
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-08 15:38:00 +0800

    add solution of problem 36

--- problems/p0036.ts
+++ problems/p0036.ts
@@ -11,9 +11,41 @@
 import { measureTime } from "../utils"
 
 function solve() {
+  const isPalindromic = (digital: number | string): boolean => {
+    const digitalStr = typeof digital === "number" ? digital.toString() : digital
+    const digitalRevertStr = digitalStr.split('').reverse().join('')
+    return digitalStr === digitalRevertStr
+  }
 
+  const decimal2Binary = (decimalNumber: number): string => {
+    if (decimalNumber === 0) {
+      return '0'
+    }
+
+    let binaryResult: string = ''
+    while (decimalNumber > 0) {
+      const remainder: number = decimalNumber % 2
+      binaryResult = remainder.toString() + binaryResult
+      decimalNumber = Math.floor(decimalNumber / 2)
+    }
+
+    return binaryResult
+  }
+
+  let sum = 0
+
+  for (let i = 1; i <= 1000000; i++) {
+    if (isPalindromic(i)) {
+      const digitalBinary = decimal2Binary(i)
+      if (isPalindromic(digitalBinary)) {
+        sum += i
+      }
+    }
+  }
+
+  return sum
 }
 
 const [result, elapsedTime] = measureTime(() => solve())
-console.log('result', result)
+console.log('result', result)//872187
 console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
