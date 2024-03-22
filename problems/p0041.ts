bf69b0f3e12807eaf9d1950b2d887935a874af01
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-23 00:00:06 +0800

    add solution of problem 41

--- problems/p0041.ts
+++ problems/p0041.ts
@@ -11,6 +11,43 @@ import { measureTime } from "../utils"
 
 function solve() {
 
+  const isPandigital = (nDigit: number): boolean => {
+    const nDigitStr = nDigit.toString()
+
+    if (nDigitStr.length != 9) {
+      return false
+    }
+
+    const nDigitSet = new Set(...nDigitStr.split(''))
+    return nDigitSet.size === 9 && !nDigitSet.has('0')
+  }
+
+  const isPrime = (n: number) => {
+    if (n <= 1) {
+      return false
+    }
+    if (n == 2) {
+      return true
+    }
+
+    const middleN = n / 2
+    for (let j = 2; j < middleN; j++) {
+      if (n % j === 0) {
+        return false
+      }
+    }
+    return true
+  }
+
+  let largest = 0
+
+  for (let i = 1; i <= 987654321; i++) {
+    if (isPandigital(i) && isPrime(i)) {
+      largest = largest > i ? largest : i
+    }
+  }
+
+  return largest
 }
 
 const [result, elapsedTime] = measureTime(() => solve())
