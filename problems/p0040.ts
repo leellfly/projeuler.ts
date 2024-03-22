b7611328075163a4902f03cd66c067130867c617
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-22 23:22:58 +0800

    add code format for problem 40

--- problems/p0040.ts
+++ problems/p0040.ts
@@ -1,13 +1,12 @@
 /*
-Champernowne's Constant
+  Champernowne's Constant
 
-An irrational decimal fraction is created by concatenating the positive integers:
-  0.12345678910[1]112131415161718192021...
-It can be seen that the 12th digit of the fractional part is 1.
+  An irrational decimal fraction is created by concatenating the positive integers:
+    0.12345678910[1]112131415161718192021...
+  It can be seen that the 12th digit of the fractional part is 1.
 
-If d(n) represents the nth digit of the fractional part, find the value of the following
-expression.
-  d(1) × d(10) × d(100) × d(1000) × d(10000) × d(100000) × d(1000000)
+  If d(n) represents the nth digit of the fractional part, find the value of the following expression.
+    d(1) × d(10) × d(100) × d(1000) × d(10000) × d(100000) × d(1000000)
 */
 
 import { measureTime } from "../utils"
@@ -25,6 +24,7 @@ function solve(): number {
 
   let concatenatedArr = concatenated.split('')
 
+  //TODD
   const d1 = +concatenatedArr[0]
   const d10 = +concatenatedArr[9]
   const d100 = +concatenatedArr[99]
