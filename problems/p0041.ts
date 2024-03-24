9527686124ae36708bb1ff386f9a15a2bf51f388
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-24 23:43:20 +0800

    add desc of problem 43

--- problems/p0041.ts
+++ problems/p0041.ts
@@ -10,45 +10,47 @@
 import { measureTime } from "../utils"
 
 function solve() {
-
-  const isPandigital = (nDigit: number): boolean => {
-    const nDigitStr = nDigit.toString()
-
-    if (nDigitStr.length != 9) {
-      return false
+  const isPandigital = (n: number): boolean => {
+    const nDigitStr = n.toString();
+    if (nDigitStr.length !== 9) {
+      return false;
     }
-
-    const nDigitSet = new Set(...nDigitStr.split(''))
-    return nDigitSet.size === 9 && !nDigitSet.has('0')
+    const nDigitSet = new Set(nDigitStr.split(''));
+    return nDigitSet.size === 9 && !nDigitSet.has('0');
   }
 
-  const isPrime = (n: number) => {
+  const isPrime = (n: number): boolean => {
     if (n <= 1) {
-      return false
+      return false;
     }
-    if (n == 2) {
-      return true
+    if (n === 2) {
+      return true;
     }
-
-    const middleN = n / 2
-    for (let j = 2; j < middleN; j++) {
+    if (n % 2 === 0) {
+      return false;
+    }
+    const sqrtN = Math.sqrt(n);
+    for (let j = 3; j <= sqrtN; j += 2) {
       if (n % j === 0) {
-        return false
+        return false;
       }
     }
-    return true
+    return true;
   }
 
-  let largest = 0
+  let largest = 0;
 
-  for (let i = 1; i <= 987654321; i++) {
+  // 减少循环范围，只遍历可能的 9 位数的倍数
+  for (let i = 987654321; i >= 123456789; i--) {
     if (isPandigital(i) && isPrime(i)) {
-      largest = largest > i ? largest : i
+      largest = i;
+      break; // 找到最大的即可退出循环
     }
   }
 
-  return largest
+  return largest;
 }
+
 
 const [result, elapsedTime] = measureTime(() => solve())
 console.log('result', result)
