305e77996bf2b25187e3ba64bb69a98f3974e760
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-26 20:19:59 +0800

    add solution of problem 42

--- problems/p0042.ts
+++ problems/p0042.ts
@@ -13,11 +13,37 @@
   Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly
   two-thousand common English words, how many are triangle words?
 */
-
+import * as fs from 'fs'
 import { measureTime } from "../utils"
 
 function solve() {
+  const letterValueMap: { [key: string]: number } = {
+    A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7, H: 8, I: 9, J: 10,
+    K: 11, L: 12, M: 13, N: 14, O: 15, P: 16, Q: 17, R: 18, S: 19,
+    T: 20, U: 21, V: 22, W: 23, X: 24, Y: 25, Z: 26,
+  }
 
+  const isTriangleNumber = (value: number) => {
+    const x1 = -0.5 + Math.sqrt(0.5 * 0.5 + 4 * 0.5 * value)
+    const x2 = -0.5 - Math.sqrt(0.5 * 0.5 + 4 * 0.5 * value)
+    return Number.isInteger(x1) || Number.isInteger(x2)
+  }
+
+  const filePath: string = "data/p0042.txt"
+
+  let wordArr: string[] = fs.readFileSync(filePath, { encoding: 'utf8', flag: 'r' })
+    .split(',')
+    .map(name => name.replace(/"/g, ''))
+
+  let count = 0
+  wordArr.forEach(word => {
+    const wordValue = word.split('').reduce((sum, word) => sum += letterValueMap[word], 0)
+    if (isTriangleNumber(wordValue)) {
+      count++
+    }
+  })
+  
+  return count
 }
 
 const [result, elapsedTime] = measureTime(() => solve())
