f469d25649cb1f62864a36ec375ec136887f4311
leellfly <20855698+leellfly@users.noreply.github.com>
2024-03-31 23:08:04 +0800

    add solution of problem 45

--- problems/p0045.ts
+++ problems/p0045.ts
@@ -13,9 +13,43 @@
 import { measureTime } from "../utils"
 
 function solve() {
+  const getTriangleNumber = (n: number): number => {
+    return n * (n + 1) / 2
+  }
 
+  const getPentagonalNumber = (n: number): number => {
+    return n * (3 * n - 1) / 2
+  }
+
+  const getHexagonalNumber = (n: number): number => {
+    return n * (2 * n - 1)
+  }
+
+  let tIndex = 286 // Starting from the next number after 285
+  let pIndex = 166
+  let hIndex = 144
+
+  let tNumber = getTriangleNumber(tIndex)
+  let pNumber = getPentagonalNumber(pIndex)
+  let hNumber = getHexagonalNumber(hIndex)
+
+  while (true) {
+    let min = Math.min(tNumber, pNumber, hNumber)
+    if (min === tNumber && min === pNumber && min === hNumber) {
+      return tNumber
+    } else if (min === tNumber) {
+      tIndex++
+      tNumber = getTriangleNumber(tIndex)
+    } else if (min === pNumber) {
+      pIndex++
+      pNumber = getPentagonalNumber(pIndex)
+    } else {
+      hIndex++
+      hNumber = getHexagonalNumber(hIndex)
+    }
+  }
 }
 
 const [result, elapsedTime] = measureTime(() => solve())
-console.log('result', result)
+console.log('result', result)//1533776805
 console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
