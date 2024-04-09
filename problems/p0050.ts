baedab935776fc1187dc5e50e8d9255f97237ae5
leellfly <20855698+leellfly@users.noreply.github.com>
2024-04-09 23:12:46 +0800

    add desc of problem 50

---
+++ problems/p0050.ts
@@ -0,0 +1,23 @@
+/*
+  Consecutive Prime Sum
+
+  The prime 41, can be written as the sum of six consecutive primes:
+    41 = 2 + 3 + 5 + 7 + 11 + 13
+
+  This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+
+  The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms,
+  and is equal to 953.
+
+  Which prime, below one-million, can be written as the sum of the most consecutive primes?
+*/
+
+import { measureTime } from "../utils"
+
+function solve() {
+
+}
+
+const [result, elapsedTime] = measureTime(() => solve())
+console.log('result', result)
+console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
