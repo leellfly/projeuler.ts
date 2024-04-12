2d8f0d27b510db0b8a9bc0e2f9475249fafe19d5
leellfly <20855698+leellfly@users.noreply.github.com>
2024-04-12 23:44:11 +0800

    add solution of problem 50

--- problems/p0050.ts
+++ problems/p0050.ts
@@ -14,10 +14,47 @@
 
 import { measureTime } from "../utils"
 
-function solve() {
+function solve(limit: number) {
+  const isPrime = (num: number): boolean => {
+    if (num <= 1) return false
+    if (num <= 3) return true
+    if (num % 2 === 0 || num % 3 === 0) return false
+    let i = 5
+    while (i * i <= num) {
+      if (num % i === 0 || num % (i + 2) === 0) return false
+      i += 6
+    }
+    return true
+  }
 
+  const findPrimesBelowLimit = (limit: number): number[] => {
+    const primes: number[] = []
+    for (let i = 2; i < limit; i++) {
+      if (isPrime(i)) {
+        primes.push(i)
+      }
+    }
+    return primes
+  }
+
+  const primes = findPrimesBelowLimit(limit)
+  let maxLength = 0
+  let maxPrime = 0
+  
+  for (let i = 0; i < primes.length; i++) {
+    let sum = 0
+    for (let j = i; j < primes.length; j++) {
+      sum += primes[j]
+      if (sum > limit) break
+      if (isPrime(sum) && j - i + 1 > maxLength) {
+        maxLength = j - i + 1
+        maxPrime = sum
+      }
+    }
+  }
+  return maxPrime
 }
 
-const [result, elapsedTime] = measureTime(() => solve())
+const [result, elapsedTime] = measureTime(() => solve(1000000))
 console.log('result', result)//997651
 console.log(`Elapsed Time: ${elapsedTime} milliseconds`)
